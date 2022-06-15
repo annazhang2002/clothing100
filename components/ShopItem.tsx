4cff384efa47b776a777ece843d81392dcd66fa3
Eden <edengorevoy@gmail.com>
2022-06-14 19:00:48 -0700

    added ShopItem component

--- components/ShopItem.tsx
+++ components/ShopItem.tsx
@@ -1,13 +1,44 @@
 import { Clothing } from "../types";
 import { View, Text } from "./Themed";
+import React from 'react';
+import { Image, StyleSheet } from 'react-native';
+
 
 // const s = require('./style');
 
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 50,
+    },
+    thumbnail: {
+        width: 150,
+        height: 150,
+    },
+    text: {
+        textAlign: "center",
+    }
+});
+
+
 export default function ShopItem({ item }: { item: Clothing }) {
     return (
-        <View>
-            <Text>{item.title}</Text>
-            <Text>hello</Text>
+        <View style={styles.container}>
+            <Image
+                style={styles.thumbnail}
+                source={{
+                    uri: item.image.url.toString(),
+                }}
+
+            />
+            <Text style={styles.text}>{item.title} {'\n'}
+                {`${getNameFromUserId(item.sellerId)}`}</Text>
         </View>
     )
+}
+
+/* TODO: Actually write the logic for this.. also, move to some utils file?
+* Given some userId, return the user's name.
+*/
+function getNameFromUserId(id: Number): string {
+    return "Jane Doe";
 }
