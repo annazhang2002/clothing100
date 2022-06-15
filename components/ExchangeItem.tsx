68543c50017630b9f0f8b1e6350cbd77f752970b
tgeller08 <tgeller08@gmail.com>
2022-06-14 23:26:44 -0400

    Added image to component

--- components/ExchangeItem.tsx
+++ components/ExchangeItem.tsx
@@ -1,15 +1,32 @@
-import { Clothing, User } from "../types";
+import { Clothing, User , ImageType} from "../types";
 import { View, Text } from "./Themed";
+import React from 'react';
+
+import {Image, StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+  },
+  tinyLogo: {
+    width: 50,
+    height: 50,
+  },
+});
 
 
-export default function ExchangeItem(props: {item: Clothing; person: User;}) {
+
+export default function ExchangeItem(props: {item: Clothing; person: User; picture: ImageType}) {
     return (
-        <View>
-            <Text>{props.person.firstName}</Text>
-            <Text>would like to exchange</Text>
-            <Text>{props.item.title}</Text>
-        </View>
-    )
-}
-
-// React native images https://reactnative.dev/docs/image
+      <View style={styles.container}>
+        <Text> {props.person.firstName} would like to  </Text>
+        <Text> {props.item.title} </Text>
+        <Image
+          style={styles.tinyLogo}
+          source={{
+            uri: props.item.image.url.toString(),
+          }}
+        />
+      </View>
+)
+};
