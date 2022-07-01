ed7166b6cf396e3564bb2961a2d6550fcb2497d2
Anna Zhang <annazhang2002@gmail.com>
2022-06-30 22:14:32 -0600

    created test objects files

---
+++ constants/TestObjects.ts
@@ -0,0 +1,42 @@
+import { User, Clothing, Image } from '../types'
+import * as ENUMS from '../typeEnums'
+
+export const testUser: User = {
+    classYear: 2024,
+    email: 'anna.zhang@yale.edu',
+    firstName: 'Anna',
+    garmentsBorrowed: [],
+    garmentsBought: [],
+    garmentsLent: [],
+    garmentsSold: [],
+    lastName: 'Zhang',
+    listBubbles: [],
+    listFriends: [],
+    numGarmentsBorrowed: 0,
+    numGarmentsBought: 0,
+    numGarmentsLent: 0,
+    numGarmentsSold: 0,
+    residentialCollege: 'Ezra Stiles',
+    userId: 1,
+    username: 'anna'
+}
+
+export const testImg: Image = {
+    url: "https://images.fcwholesale.com/TP/6/TP6503-RED-03.jpg",
+    altText: "cool shirt"
+}
+
+export const testClothing: Clothing = {
+    available: true,
+    title: "A Very Cool Shirt",
+    id: 1,
+    image: testImg,
+    sellerId: 1,
+    category: "hi",
+    offerType: ENUMS.ClothingOfferType.Borrow,
+    color: [ENUMS.ClothingColor.Black],
+    size: ENUMS.ClothingSize.L,
+    description: "this is a very cool shirt",
+    condition: ENUMS.ClothingCondition.BrandNew,
+    brand: "h",
+}
--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -3,30 +3,15 @@ import { StyleSheet, Button } from 'react-native';
 import EditScreenInfo from '../components/EditScreenInfo';
 import { Text, View } from '../components/Themed';
 import { RootTabScreenProps, Clothing, Image } from '../types';
-import * as ENUMS from '../typeEnums'
 import { firebase } from '../config';
+
+import { testClothing } from '../constants/TestObjects'
 
 export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'>) {
 
   const clothesRef = firebase.firestore().collection('clothes')
 
-  const shirtImg: Image = {
-    url: "https://images.fcwholesale.com/TP/6/TP6503-RED-03.jpg",
-    altText: "cool shirt"
-  }
-  const item: Clothing = {
-    title: "A Very Cool Shirt",
-    id: 1,
-    image: shirtImg,
-    sellerId: 1,
-    category: "hi",
-    offerType: ENUMS.ClothingOfferType.Borrow,
-    color: [ENUMS.ClothingColor.Black],
-    size: ENUMS.ClothingSize.L,
-    description: "this is a very cool shirt",
-    condition: ENUMS.ClothingCondition.BrandNew,
-    brand: "h",
-  }
+
   const addClothing = (item: Clothing) => {
     clothesRef.add(item)
       .then(doc => {
@@ -41,7 +26,7 @@ export default function TabOneScreen({ navigation }: RootTabScreenProps<'TabOne'
       <Text style={styles.title}>Tab One</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-      <Button title="Add Clothing" onPress={() => addClothing(item)} />
+      <Button title="Add Clothing" onPress={() => addClothing(testClothing)} />
     </View>
   );
 }
