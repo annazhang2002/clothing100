817d7a3d25b23a954ffd65f1a727f8ebfde52aeb
Anna Zhang <annazhang2002@gmail.com>
2022-07-19 19:45:55 -0600

    added redux function to add a user to a bubble (#46)

--- redux/actions/bubbles.tsx
+++ redux/actions/bubbles.tsx
@@ -1,17 +1,14 @@
 import { BUBBLES_ACTIONS } from "../constants/bubbles";
 import { firebase } from '../../config';
 import { Dispatch } from 'redux';
-import { Bubble, BubblesById } from "../../types";
+import { Bubble, BubblesById, User } from "../../types";
 
 const bubbleRef = firebase.firestore().collection('bubbles')
 
 export const createBubble = (newBubble: Bubble) => {
-    console.log("-----------CREATING BUBBLES----------")
     return (dispatch: Dispatch) => {
         return bubbleRef.add(newBubble)
             .then(res => {
-                console.log("RES", res)
-                console.log("RES.ID", res.id)
                 dispatch(
                     { type: BUBBLES_ACTIONS.CREATE_BUBBLE, payload: { bubble: newBubble, id: res.id } }
                 )
@@ -23,7 +20,6 @@ export const createBubble = (newBubble: Bubble) => {
 
 export const fetchBubbles = (userId: String) => {
     return (dispatch: Dispatch) => {
-        console.log("-----------FETCHING BUBBLES----------")
         return bubbleRef.where('userIds', 'array-contains', userId).orderBy('name', 'desc')
             .onSnapshot(
                 (querySnapshot: any) => {
@@ -35,13 +31,26 @@ export const fetchBubbles = (userId: String) => {
                         bubblesIds.push(bubbleId);
                         bubblesById[bubbleId] = bubbleData
                     });
-                    console.log(bubblesById)
-                    console.log(bubblesIds)
+                    // console.log(bubblesById)
+                    // console.log(bubblesIds)
                     dispatch({ type: BUBBLES_ACTIONS.FETCH_BUBBLES, payload: { bubblesById, bubblesIds } })
                 },
                 (err: any) => {
                     dispatch({ type: BUBBLES_ACTIONS.FETCH_BUBBLES_ERROR, msg: err })
                 }
             )
+    }
+}
+
+export const addUserToBubble = (bubbleId: String, userId: String) => {
+    return (dispatch: Dispatch) => {
+        return bubbleRef.doc(bubbleId.toString()).update({ userIds: firebase.firestore.FieldValue.arrayUnion(userId) })
+            .then(() => {
+                dispatch(
+                    { type: BUBBLES_ACTIONS.ADD_USER_TO_BUBBLE, payload: { bubbleId, userId } }
+                )
+            })
+            .catch(err => dispatch(
+                { type: BUBBLES_ACTIONS.ADD_USER_TO_BUBBLE_ERROR, msg: err }))
     }
 }
