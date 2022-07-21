c4acff8659a7c979b806b275e2ec7ff2e0fb6926
Anna Zhang <annazhang2002@gmail.com>
2022-07-20 20:05:54 -0600

    [#9] made basic shopitem component (#18)
    * made basic shopitem component
    
    * added img from main merge
    
    * added ShopItem component
    
    * changed text styling
    
    * updated styles and merge conflicts
    
    Co-authored-by: Eden <edengorevoy@gmail.com>

---
+++ components/ShopItem.tsx
@@ -0,0 +1,48 @@
+import { Clothing } from "../types";
+import { View, Text } from "./Themed";
+import React from 'react';
+import { Image, StyleSheet } from 'react-native';
+
+export default function ShopItem({ item }: { item: Clothing }) {
+    return (
+        <View style={styles.container}>
+            <Image
+                style={styles.thumbnail}
+                source={{
+                    uri: item.image.url.toString(),
+                }}
+
+            />
+            <Text style={styles.baseText}>
+                <Text style={styles.itemText}>
+                    {item.title}
+                </Text>
+                {'\n'}
+                {`${getNameFromUserId(item.sellerId)}`}
+            </Text>
+        </View>
+    )
+}
+
+/* TODO: Actually write the logic for this.. also, move to some utils file?
+* Given some userId, return the user's name.
+*/
+function getNameFromUserId(id: Number): string {
+    return "Jane Doe";
+}
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 50,
+    },
+    thumbnail: {
+        width: 150,
+        height: 150,
+    },
+    baseText: {
+        textAlign: "left",
+    },
+    itemText: {
+        fontWeight: "bold",
+    }
+});
