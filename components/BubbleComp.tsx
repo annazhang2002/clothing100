6dabd2ca43d6a12a6dfcf4e881125b3db8f48557
Anna Zhang <annazhang2002@gmail.com>
2022-07-20 19:41:32 -0600

    Make bubble page (#47)
    * created flatlist for bubbles
    
    * added button to add new bubble

--- components/BubbleComp.tsx
+++ components/BubbleComp.tsx
@@ -2,25 +2,28 @@ import { Bubble } from "../types";
 import { View, Text } from "./Themed";
 import { StyleSheet } from 'react-native';
 
-export default function BubbleComp({ item }: {item: Bubble}) {
+export default function BubbleComp({ item }: { item: Bubble }) {
     return (
-        <View style={[styles.circle, {backgroundColor: item.color }]}>
-                <Text style={styles.text}>{item.name}</Text>
+        <View style={[styles.circle, { backgroundColor: item.color }]}>
+            <Text style={styles.text}>{item.name}</Text>
         </View>
     );
 }
 
 const styles = StyleSheet.create({
     circle: {
-      height: 70,
-      width: 70,
-      borderRadius: 35,
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center'
+        height: 120,
+        width: 120,
+        borderRadius: 75,
+        margin: 20,
+        padding: 10,
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     text: {
         textAlign: 'center',
-        color: 'black'
+        color: 'black',
+        fontSize: 20,
     }
 });
--- screens/BubblesTabScreen.tsx
+++ screens/BubblesTabScreen.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, Button } from 'react-native';
+import { StyleSheet, Button, ScrollView, FlatList, TouchableOpacity } from 'react-native';
 
 import { Text, View } from '../components/Themed';
 import BubbleComp from '../components/BubbleComp';
@@ -8,24 +8,45 @@ import { Bubble } from '../types';
 import { createBubble, fetchBubbles, addUserToBubble } from '../redux/actions/bubbles';
 import { connect } from 'react-redux';
 import { useEffect } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faPlus } from '@fortawesome/free-solid-svg-icons/faPlus'
 
 function BubblesTabScreen(props: any) {
 
     // TODO: this is running when the props update i think? we should try putting it into a higher class
     useEffect(() => {
         props.fetchBubbles('a1')
+        // console.log(props.bubblesIds)
     }, [])
 
     return (
         <View style={styles.container}>
-            <Text style={styles.title}>Bubbles</Text>
-            <Button title="create bubble" onPress={() => props.createBubble(testBubble)} />
-            <Button title="add user to bubble" onPress={() => props.addUserToBubble(props.bubblesIds[1], 'w1')} />
-            {props.bubblesIds.map((bubbleId: String) => {
-                return (
-                    <BubbleComp item={props.bubblesById[bubbleId.toString()]} />
-                )
-            })}
+            <Text style={styles.title}>My Bubbles</Text>
+            <TouchableOpacity
+                activeOpacity={0.5}
+                onPress={() => props.createBubble(testBubble)}
+                style={styles.addButton}
+            >
+                <FontAwesomeIcon icon={faPlus} color={'white'} size={20} />
+            </TouchableOpacity>
+            {/* <Button title="create bubble" onPress={() => props.createBubble(testBubble)} /> */}
+            {/* <Button title="add user to bubble" onPress={() => props.addUserToBubble(props.bubblesIds[1], 'w1')} /> */}
+            <ScrollView
+                showsVerticalScrollIndicator={false}
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={{
+                    flexDirection: 'row',
+                    flexWrap: 'wrap',
+                }}>
+                <FlatList
+                    data={props.bubblesIds}
+                    // renderItem={({ item }) => (<Text>{item}</Text>)}
+                    renderItem={({ item }) => (<BubbleComp item={props.bubblesById[item.toString()]} />)}
+                    keyExtractor={item => `${item}`}
+                    showsHorizontalScrollIndicator={false}
+                    numColumns={2}
+                />
+            </ScrollView>
         </View>
     );
 }
@@ -35,6 +56,7 @@ const styles = StyleSheet.create({
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center',
+        padding: 20
     },
     title: {
         fontSize: 20,
@@ -45,6 +67,11 @@ const styles = StyleSheet.create({
         height: 1,
         width: '80%',
     },
+    addButton: {
+        position: 'absolute',
+        top: 20,
+        right: 20,
+    }
 });
 
 const mapStateToProps = (state: any) => ({
