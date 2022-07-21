d3bfa68756af5952c7451fcb9d8ab89ce35f4852
Anna Zhang <annazhang2002@gmail.com>
2022-07-20 19:58:25 -0600

    made add clothing item reducer (#48)

---
+++ redux/reducers/clothes.tsx
@@ -0,0 +1,28 @@
+import { CLOTHING_ACTIONS } from "../constants/clothes";
+
+var _ = require('lodash');
+
+const initialState = {
+    clothesById: {},
+    clothesIds: [],
+    error: ''
+};
+
+const clothesReducer = (state = initialState, action: any) => {
+    switch (action.type) {
+        case CLOTHING_ACTIONS.ADD_CLOTHING: {
+            const id = action.payload.id
+            const item = action.payload.item
+            return { ...state, clothesById: _.set(state.clothesById, id, item), clothesIds: _.concat([id], state.clothesIds) };
+        }
+        case CLOTHING_ACTIONS.ADD_CLOTHING_ERROR: {
+            console.log("ERROR ADDING CLOTHING: ", action.msg)
+            return { ...state, error: action.msg }
+        }
+        default: {
+            return state;
+        }
+    }
+}
+
+export default clothesReducer;
--- redux/store.js
+++ redux/store.js
@@ -3,11 +3,12 @@ import userReducer from './reducers/user';
 import bubblesReducer from './reducers/bubbles';
 import thunk from 'redux-thunk';
 import exchangeReducer from "./reducers/exchange";
+import clothesReducer from "./reducers/clothes";
 
 const middlewares = [thunk];
 
 const rootReducer = combineReducers(
-    { user: userReducer, bubbles: bubblesReducer, exchange: exchangeReducer },
+    { user: userReducer, bubbles: bubblesReducer, exchange: exchangeReducer, clothes: clothesReducer },
 );
 const configureStore = () => {
     return createStore(rootReducer, applyMiddleware(...middlewares));
